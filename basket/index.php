a98cb9793e20fec6cd6f2f7566cd6d5c1e1fe57d
Unfabul <yoteam@yandex.ru>
2019-03-26 18:57:58 +0300

    Вывод добавленных товаров из сессии + их кол-во

--- basket/index.php
+++ basket/index.php
@@ -2,7 +2,8 @@
     $pageConfig = [
         'title' => 'Корзина',
         'cssFiles' => [
-            '/css/dist/main.css'
+            '/css/dist/main.css',
+            '/css/dist/basket.css'
         ],
         'jsFiles' => [
             '/js/script.js'
@@ -21,9 +22,35 @@
         $result = mysqli_query($db, $sql);
         $product = mysqli_fetch_assoc($result);
 
+        $product['count'] = $basketItem['count'];
+
         $template['products'][] = $product;
     }
 ?>
+
+<!-- new -->
+<main class="main">
+    <h1 class="main-header">ВАША КОРЗИНА</h1>
+        <div class="basket-header">
+            <div class="photo">Фото</div>
+            <div class="name">Наименование</div>
+            <div class="count">Кол-во</div>
+            <div class="price">Цена</div>
+        </div>
+        <?php foreach($template['products'] as $productItem):?>
+        <div class="basket">
+            <div class="basket__img" style='background-image:url(<?=$productItem['photo']?>)'></div>
+            <div class="basket__name"><?=$productItem['name']?></div>
+            <div class="basket__count"><?=$productItem['count']?></div>
+            <?php
+                $price = ($productItem['price'] - $productItem['price'] * $productItem['sale']) * $productItem['count'];
+            ?>
+            <div class="basket__price"><?=$price?> руб.</div>
+        </div>
+        <?php endforeach; ?>
+    
+</main>
+<!-- new -->
 
 <?php 
     include($_SERVER['DOCUMENT_ROOT'].'/parts/footer.php');
